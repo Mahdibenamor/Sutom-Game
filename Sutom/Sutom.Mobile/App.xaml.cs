0d4170565ae79824ca9a01fa59218c21e47df6f1
Mahdi Ben Amor <mahdi.benamor@list.lu>
2024-06-28 21:42:16 +0200

    adding empty mobile app

---
+++ Sutom/Sutom.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sutom.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
